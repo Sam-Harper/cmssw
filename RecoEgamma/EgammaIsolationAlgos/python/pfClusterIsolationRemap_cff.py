cc23ed88b0afb4a2ad9de2e46e6a095505bda530
Sam Harper <sam.j.harper@gmail.com>
2017-01-30 22:14:55 +0000

    fixing AOD ID sequences

--- RecoEgamma/EgammaIsolationAlgos/python/pfClusterIsolationRemap_cff.py
+++ RecoEgamma/EgammaIsolationAlgos/python/pfClusterIsolationRemap_cff.py
@@ -1,26 +1,26 @@
 import FWCore.ParameterSet.Config as cms
 
 electronHcalPFClusterIsolationRemapper = cms.EDProducer('ElectronPFClusterIsolationRemapper',
-    candidateProducer = cms.InputTag('gsFixedGsfElectrons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGsfElectrons'),
+    candidateProducer = cms.InputTag('gedGsfElectronsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedGsfElectronsGSFixed'),
     isolationMap = cms.InputTag('electronHcalPFClusterIsolationProducer', '', cms.InputTag.skipCurrentProcess())
 )
 
 photonHcalPFClusterIsolationRemapper = cms.EDProducer('PhotonPFClusterIsolationRemapper',
-    candidateProducer = cms.InputTag('gsFixedGEDPhotons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGEDPhotons'),
+    candidateProducer = cms.InputTag('gedPhotonsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedPhotonsGSFixed'),
     isolationMap = cms.InputTag('photonHcalPFClusterIsolationProducer', '', cms.InputTag.skipCurrentProcess())
 )
 
 electronEcalPFClusterIsolationRemapper = cms.EDProducer('ElectronPFClusterIsolationRemapper',
-    candidateProducer = cms.InputTag('gsFixedGsfElectrons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGsfElectrons'),
+    candidateProducer = cms.InputTag('gedGsfElectronsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedGsfElectronsGSFixed'),
     isolationMap = cms.InputTag('electronEcalPFClusterIsolationProducer', '', cms.InputTag.skipCurrentProcess())
 )
 
 photonEcalPFClusterIsolationRemapper = cms.EDProducer('PhotonPFClusterIsolationRemapper',
-    candidateProducer = cms.InputTag('gsFixedGEDPhotons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGEDPhotons'),
+    candidateProducer = cms.InputTag('gedPhotonsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedPhotonsGSFixed'),
     isolationMap = cms.InputTag('photonEcalPFClusterIsolationProducer', '', cms.InputTag.skipCurrentProcess())
 )
 
--- RecoEgamma/ElectronIdentification/python/idExternalRemap_cfi.py
+++ RecoEgamma/ElectronIdentification/python/idExternalRemap_cfi.py
@@ -1,50 +1,50 @@
 import FWCore.ParameterSet.Config as cms
 
 eidLoose = cms.EDProducer('ElectronIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGsfElectrons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGsfElectrons'),
+    candidateProducer = cms.InputTag('gedGsfElectronsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedGsfElectronsGSFixed'),
     idMap = cms.InputTag('eidLoose', '', cms.InputTag.skipCurrentProcess())
 )
 
 eidRobustHighEnergy = cms.EDProducer('ElectronIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGsfElectrons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGsfElectrons'),
+    candidateProducer = cms.InputTag('gedGsfElectronsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedGsfElectronsGSFixed'),
     idMap = cms.InputTag('eidRobustHighEnergy', '', cms.InputTag.skipCurrentProcess())
 )
 
 eidRobustLoose = cms.EDProducer('ElectronIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGsfElectrons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGsfElectrons'),
+    candidateProducer = cms.InputTag('gedGsfElectronsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedGsfElectronsGSFixed'),
     idMap = cms.InputTag('eidRobustLoose', '', cms.InputTag.skipCurrentProcess())
 )
 
 eidRobustTight = cms.EDProducer('ElectronIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGsfElectrons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGsfElectrons'),
+    candidateProducer = cms.InputTag('gedGsfElectronsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedGsfElectronsGSFixed'),
     idMap = cms.InputTag('eidRobustTight', '', cms.InputTag.skipCurrentProcess())
 )
 
 eidTight = cms.EDProducer('ElectronIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGsfElectrons'),
-    newToOldObjectMap = cms.InputTag('gsFixedGsfElectrons'),
+    candidateProducer = cms.InputTag('gedGsfElectronsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedGsfElectronsGSFixed'),
     idMap = cms.InputTag('eidTight', '', cms.InputTag.skipCurrentProcess())
 )
 
 PhotonCutBasedIDLoose = cms.EDProducer('PhotonIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGedPhoton'),
-    newToOldObjectMap = cms.InputTag('gsFixedGedPhoton'),
+    candidateProducer = cms.InputTag('gedPhotonsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedPhotonsGSFixed'),
     idMap = cms.InputTag('PhotonIDProdGED', 'PhotonCutBasedIDLoose', cms.InputTag.skipCurrentProcess())
 )
 
 PhotonCutBasedIDLooseEM = cms.EDProducer('PhotonIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGedPhoton'),
-    newToOldObjectMap = cms.InputTag('gsFixedGedPhoton'),
+    candidateProducer = cms.InputTag('gedPhotonsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedPhotonsGSFixed'),
     idMap = cms.InputTag('PhotonIDProdGED', 'PhotonCutBasedIDLooseEM', cms.InputTag.skipCurrentProcess())
 )
 
 PhotonCutBasedIDTight = cms.EDProducer('PhotonIDExternalProducerRemapper',
-    candidateProducer = cms.InputTag('gsFixedGedPhoton'),
-    newToOldObjectMap = cms.InputTag('gsFixedGedPhoton'),
+    candidateProducer = cms.InputTag('gedPhotonsGSFixed'),
+    newToOldObjectMap = cms.InputTag('gedPhotonsGSFixed'),
     idMap = cms.InputTag('PhotonIDProdGED', 'PhotonCutBasedIDTight', cms.InputTag.skipCurrentProcess())
 )
 
