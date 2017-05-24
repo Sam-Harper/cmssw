47b0813639f37bc80ca4f5c639077959315f2760
Sam Harper <sam.j.harper@gmail.com>
2017-05-24 12:02:33 +0100

    first compiles although not all code being generated

--- DQMOffline/Trigger/interface/HLTTagAndProbeEff.h
+++ DQMOffline/Trigger/interface/HLTTagAndProbeEff.h
@@ -3,15 +3,20 @@
 
 
 #include "FWCore/Common/interface/TriggerNames.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
 
 #include "DQMServices/Core/interface/DQMStore.h"
 #include "DQMServices/Core/interface/MonitorElement.h"
 
-
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
 #include "DataFormats/HLTReco/interface/TriggerEvent.h"
 #include "DataFormats/HLTReco/interface/TriggerObject.h"
 #include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
+#include "DataFormats/Math/interface/deltaR.h"
+
+#include <boost/algorithm/string.hpp>
 
 #include <vector>
 #include <string>
@@ -27,35 +32,40 @@ namespace{
 
 namespace{
   bool passTrig(const float objEta,float objPhi, const trigger::TriggerEvent& trigEvt,
-		const std::string & filterNames){
+		const std::string & filterName,const std::string& processName){
     constexpr float kMaxDR2 = 0.1*0.1;
     
-    edm::InputTag filterTag(filterName,"","HLTX"); 
+    edm::InputTag filterTag(filterName,"",processName); 
     trigger::size_type filterIndex = trigEvt.filterIndex(filterTag); 
-    if(filterIndex<trigEvent.sizeFilters()){ //check that filter is in triggerEvent
+    if(filterIndex<trigEvt.sizeFilters()){ //check that filter is in triggerEvent
       const trigger::Keys& trigKeys = trigEvt.filterKeys(filterIndex); 
       const trigger::TriggerObjectCollection & trigObjColl(trigEvt.getObjects());
       for(trigger::Keys::const_iterator keyIt=trigKeys.begin();keyIt!=trigKeys.end();++keyIt){ 
 	const trigger::TriggerObject& trigObj = trigObjColl[*keyIt];
-	if(reco::deltaR2(trigObj.eta(),trigObj.phi(),objEta,objPhi)<kMaxDR) return true;
+	if(reco::deltaR2(trigObj.eta(),trigObj.phi(),objEta,objPhi)<kMaxDR2) return true;
       }
     }
     return false;
   }
   bool passTrig(const float objEta,float objPhi, const trigger::TriggerEvent& trigEvt,
-		const std::vector<std::string>& filterNames){
+		const std::vector<std::string>& filterNames,const std::string& processName){
     for(auto& filterName : filterNames){
-      if(!passTrig(objEta,objPhi,trigEvt,filterName)) return false;
+      if(!passTrig(objEta,objPhi,trigEvt,filterName,processName)) return false;
     }
     return true;
+  }
+  template<typename T> edm::Handle<T> getHandle(const edm::Event& event,const edm::EDGetTokenT<T>& token){
+    edm::Handle<T> handle;
+    event.getByToken(token,handle);
+    return handle;
   }
 }
 
 //our base class for our histograms
 //takes an object, edm::Event,edm::EventSetup and fills the histogram
 //with the predetermined variable (or varaibles) 
-template <typename ObjType,typename ValType> 
-class HLTDQM1DHist : public HLTDQMHist {
+template <typename ObjType> 
+class HLTDQMHist {
 public:
   HLTDQMHist()=default;
   virtual ~HLTDQMHist()=default;
@@ -68,7 +78,7 @@ public:
 //and the histogram itself
 //we do not own the histogram
 template <typename ObjType,typename ValType> 
-class HLTDQM1DHist : public HLTDQMHist {
+class HLTDQM1DHist : public HLTDQMHist<ObjType> {
 public:
   void fill(const ObjType& objType,const edm::Event& event,const edm::EventSetup& setup)override{
     hist_->Fill(var_(objType));
@@ -88,7 +98,7 @@ public:
   void fillHists(const ObjType& objType,const edm::Event& event,const edm::EventSetup& setup);
 private:
 
-  std::vector<std::unique_ptr<HLTDQMHist> > hists_;
+  std::vector<std::unique_ptr<HLTDQMHist<ObjType> > > hists_;
   std::string filterName_;
   std::string baseHistName_;
   edm::ParameterSet histsConfig_;		 
@@ -101,20 +111,20 @@ void HLTDQMHistColl<ObjType>::bookHists(DQMStore::IBooker& iBooker,
 {
   auto histConfigs = config.getParameter<std::vector<edm::ParameterSet> >("hists");
   for(auto& histConfig : histConfigs){
-    auto binLowEdges = histConfig.getParameters<std::vector<double> >("binLowEdges");
+    auto binLowEdges = histConfig.getParameter<std::vector<double> >("binLowEdges");
     auto nameSuffex = histConfig.getParameter<std::string>("nameSuffex");
     auto me = iBooker.book1D((baseName+nameSuffex).c_str(),(baseName+nameSuffex).c_str(),
 			     binLowEdges.size()-1,&binLowEdges[0]);
-    std::unique_ptr<HLTDQMHist> hist;
+    std::unique_ptr<HLTDQMHist<ObjType> > hist;
     auto vsVar = histConfig.getParameter<std::string>("vsVar");
     switch(vsVar){
-    case "et":hist = std::make_unique_ptr<HLTDQM1DHist<ObjType,float> >(me->getTH1(),ObjType::et);
+    case "et":hist = std::make_unique<HLTDQM1DHist<ObjType,float> >(me->getTH1(),ObjType::et);
       break;
-    case "pt" : hist = std::make_unique_ptr<HLTDQM1DHist<ObjType,float> >(me->getTH1(),ObjType::pt);
+    case "pt" : hist = std::make_unique<HLTDQM1DHist<ObjType,float> >(me->getTH1(),ObjType::pt);
       break;
-    case "eta": hist = std::make_unique_ptr<HLTDQM1DHist<ObjType,float> >(me->getTH1(),ObjType::eta);
+    case "eta": hist = std::make_unique<HLTDQM1DHist<ObjType,float> >(me->getTH1(),ObjType::eta);
       break;
-    case "scEta": hist = std::make_unique_ptr<HLTDQM1DHist<ObjType,float> >(me->getTH1(),scEtaFunc);
+    case "scEta": hist = std::make_unique<HLTDQM1DHist<ObjType,float> >(me->getTH1(),scEtaFunc);
       break;
     default:
       throw cms::Exception("ConfigError") <<" vsVar "<<vsVar<<" not recognised"<<std::endl;
@@ -143,11 +153,11 @@ class RangeCuts {
       break;
     case "pt": varFunc_ = &ObjType::pt;
       break;
-    case "eta": varFunc_ = &ObjType::eta:
+    case "eta": varFunc_ = &ObjType::eta;
       break;
     case "scEta": varFunc_ = &scEtaFunc;
       break;
-    case: "": //empty string, allow it to auto pass
+    case "": //empty string, allow it to auto pass
       break;
     default:
       throw cms::Exception("ConfigError") <<" rangeVar "<<varName<<" not recognised"<<std::endl;
@@ -160,11 +170,11 @@ class RangeCuts {
       allowedRanges_.push_back({std::stof(splitRange[0]),std::stof(splitRange[1])});
     }
   }
-  bool operator(const ObjType& obj)const{
+  bool operator()(const ObjType& obj)const{
     if(!varFunc_) return true; //auto pass if we dont specify a variable function
     else{ 
       float varVal = varFunc_(obj);
-      for(auto& range : allowedRanges){
+      for(auto& range : allowedRanges_){
 	if(varVal>=range.first && varVal<range.second) return true;
       }
       return false;
@@ -179,18 +189,18 @@ template<typename ObjType>
 class RangeCutsColl {
 public:
   explicit RangeCutsColl(edm::ParameterSet& config){
-    const auto cutsConfig config.getParameter<vector<edm::ParameterSet> >("cuts");
-    for(const auto & cutConfig : cutsConfig) rangeCuts_.emplace_back(RangeCuts(cutConfig));
+    const auto cutsConfig = config.getParameter<std::vector<edm::ParameterSet> >("cuts");
+    for(const auto & cutConfig : cutsConfig) rangeCuts_.emplace_back(RangeCuts<ObjType>(cutConfig));
   }
   //if no cuts are defined, it returns true
-  bool operator(const ObjType& obj)const{
+  bool operator()(const ObjType& obj)const{
     for(auto& cut : rangeCuts_){
       if(!cut(obj)) return false;
     }
     return true;
   }
 private:
-  std::vector<RangeCuts<ObjType> rangeCuts_;
+  std::vector<RangeCuts<ObjType> > rangeCuts_;
 };
 
 template <typename ObjType,typename ObjCollType> 
@@ -203,20 +213,27 @@ public:
   void fill(const edm::Event& event,const edm::EventSetup& setup);
   
 private:
+  std::vector<edm::Ref<ObjType> >
+  getPassingRefs(edm::Handle<ObjCollType>& objCollHandle,
+		 edm::Handle<bool>& vidHandle,
+		 const trigger::TriggerEvent& trigEvt,
+		 const std::vector<std::string>& filterNames,
+		 const RangeCutsColl<ObjType>& rangeCuts);
+private:
   edm::EDGetTokenT<ObjCollType> objToken_;
-  edm::EDGetTokenT<TriggerEvent> trigEvtToken_;
-  edm::EDGetTokenT<TriggerResults> trigResultsToken_;
+  edm::EDGetTokenT<trigger::TriggerEvent> trigEvtToken_;
+  edm::EDGetTokenT<edm::TriggerResults> trigResultsToken_;
+  edm::EDGetTokenT<edm::ValueMap<bool> >tagVIDToken_;
+  edm::EDGetTokenT<edm::ValueMap<bool> >probeVIDToken_;
   
   std::string hltProcess_;
 
   std::string tagTrigger_;
 	    
   std::vector<std::string> tagFilters_;
-  std::string tagVIDCuts_;
-  RangeCutsColl tagRangeCuts_;
+  RangeCutsColl<ObjType> tagRangeCuts_;
 
-  std::vector<std::string> probeFilter_;
-  std::string probeVIDCuts_;
+  std::vector<std::string> probeFilters_;
   RangeCutsColl<ObjType> probeRangeCuts_;
 
   float minMass_;
@@ -230,29 +247,27 @@ private:
 template <typename ObjType,typename ObjCollType> 
 HLTTagAndProbeEff<ObjType,ObjCollType>::HLTTagAndProbeEff(const edm::ParameterSet& pset,edm::ConsumesCollector && cc)
 {
-
   edm::InputTag trigEvtTag = pset.getParameter<edm::InputTag>("trigEvent");
 
   objToken_ = cc.consumes<ObjCollType>(pset.getParameter<edm::InputTag>("objColl"));
-  trigEvtToken_ = cc.consumes<ObjCollType>(trigEvtTag);
-  trigResultsToken_ = cc.consumes<ObjCollType>(pset.getParameter<edm::InputTag>("trigResults"));
+  trigEvtToken_ = cc.consumes<trigger::TriggerEvent>(trigEvtTag);
+  trigResultsToken_ = cc.consumes<edm::TriggerResults>(pset.getParameter<edm::InputTag>("trigResults"));
+  tagVIDToken_ = cc.consumes<edm::ValueMap<bool> >(pset.getParameter<edm::InputTag>("tagVIDCuts"));
+  probeVIDToken_ = cc.consumes<edm::ValueMap<bool> >(pset.getParameter<edm::InputTag>("probeVIDCuts"));
 
   hltProcess_ = trigEvtTag.process();
 
   tagTrigger_ = pset.getParameter<std::string>("tagTrigger");
+
   tagFilters_ = pset.getParameter<std::vector<std::string> >("tagFilters");
-  tagVIDCuts_ = pset.getParameter<std::string>("tagVIDCuts");
   tagRangeCuts_ = pset.getParameter<edm::ParameterSet>("tagRangeCuts");
 
   probeFilters_ = pset.getParameter<std::vector<std::string> >("probeFilters");
-  probeVIDCuts_ = pset.getParameter<std::string>("probeVIDCuts");
   probeRangeCuts_ = pset.getParameter<edm::ParameterSet>("probeRangeCuts");
   
   minMass_ = pset.getParameter<double>("minMass");
   maxMass_ = pset.getParameter<double>("maxMass");
-  requireOpSign = pset.getParameter<bool>("requireOpSign");
-  
-  
+  requireOpSign_ = pset.getParameter<bool>("requireOpSign");
 }
 
 template <typename ObjType,typename ObjCollType> 
@@ -260,16 +275,15 @@ std::vector<edm::Ref<ObjType> > HLTTagAndProbeEff<ObjType,ObjCollType>::
 getPassingRefs(edm::Handle<ObjCollType>& objCollHandle,
 	       edm::Handle<bool>& vidHandle,
 	       const trigger::TriggerEvent& trigEvt,
-	       const std::vector<std::string> filterNames,
+	       const std::vector<std::string>& filterNames,
 	       const RangeCutsColl<ObjType>& rangeCuts)
 {
   std::vector<edm::Ref<ObjType> > passingRefs;
   for(size_t objNr=0;objNr<objCollHandle->size();objNr++){
     edm::Ref<ObjType> ref(objCollHandle,objNr);
-    
     if(rangeCuts(*ref) && 
-       passTrig(*ref,trigEvt,filterNames) && 
-       (!vidHandle->isValid() || (*vidHandle)[ref]==true)){
+       passTrig(ref->eta(),ref->phi(),trigEvt,filterNames,hltProcess_) && 
+       (vidHandle.isValid()==false || (*vidHandle)[ref]==true)){
       passingRefs.push_back(ref);
       
     }
@@ -277,8 +291,8 @@ getPassingRefs(edm::Handle<ObjCollType>& objCollHandle,
   return passingRefs;
 }
 
-template <typename ObjType> 
-void HLTTagAndProbeEff<ObjType>::fill(const edm::Event& event,const edm::EventSetup& setup)
+template <typename ObjType,typename ObjCollType> 
+void HLTTagAndProbeEff<ObjType,ObjCollType>::fill(const edm::Event& event,const edm::EventSetup& setup)
 {
   auto objCollHandle = getHandle(event,objToken_); 
   auto trigEvtHandle = getHandle(event,trigEvtToken_);
@@ -298,17 +312,17 @@ void HLTTagAndProbeEff<ObjType>::fill(const edm::Event& event,const edm::EventSe
 							     probeFilters_,
 							     probeVIDHandle,probeRangeCuts_);
   
-  for(auto& tagRef : tagsRefs){
+  for(auto& tagRef : tagRefs){
     for(auto& probeRef : probeRefs){
       if(tagRef==probeRef) continue; //otherwise its the same object...
       float mass = (tagRef->p4()+probeRef->p4()).mag();
       
       if( ( mass>minMass_ || minMass_<0 ) && 
 	  ( mass<maxMass_ || maxMass_<0 ) && 
-	  ( !requireOpSign || tagRef->charge()!=probeRef->charge()) ){
+	  ( !requireOpSign_ || tagRef->charge()!=probeRef->charge()) ){
 
 	for(auto& histColl : histColls_){
-	  histColl.fill(probeRef,event,setup,*trigSumHandle);
+	  histColl.fill(probeRef,event,setup,*trigEvtHandle);
 	}
 	      
       }//end of t&p pair cuts
